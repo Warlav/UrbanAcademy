a1cf495b95c81a1febd1503f569e9392c4969995
Warlav <warlav@gmail.com>
2024-08-09 02:58:34 +0300

    Update module_1_3.py

--- module_1_3.py
+++ module_1_3.py
@@ -1,1 +1,8 @@
-
+name = 'Паша'
+print('Имя: ', name)
+age = int(39)
+print('Возсраст: ', age)
+age = age + 10
+print('Новый возраст: ', age)
+is_student = bool(True)
+print('Студент: ', is_student)
