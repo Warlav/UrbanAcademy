61bdf5b717c9b934607045816cc258c40567db39
Warlav <warlav@gmail.com>
2024-08-11 12:34:17 +0300

    Update module_1_5.py

--- module_1_5.py
+++ module_1_5.py
@@ -1,2 +1,7 @@
-immutable_var = (1, 2, 3, 4, 5, Вышел, зайчик, погулять)
+immutable_var = (1, 2, 3, 4, 5, 'Вышел', 'зайчик', 'погулять')
 print(immutable_var)
+#immutable_var[0] = 'Раз' # Облом, потому что кортеж является неизменяемым.
+mutable_list = ['Вдруг', 0, 'хотник', 'выбегает', 'прямо в', 3, 'айчика', 'стреляет']
+mutable_list[1] = 'о'
+mutable_list[5] = 'з'
+print(mutable_list)
