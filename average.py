882ed159d718f7a5c644bb44fa21c0353bc66a6c
Warlav <warlav@gmail.com>
2024-08-14 21:10:39 +0300

    Create average.py

---
+++ average.py
@@ -0,0 +1,11 @@
+grades = [[5,3,3,5,4],[2,2,2,3],[4,5,5,2],[4,4,3],[5,5,5,4,5]]
+students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
+average1 = sum(grades[0])/len(grades[0])          #средняя оценка первого студента
+average2 = sum(grades[1])/len(grades[1])          #средняя оценка второго студента
+average3 = sum(grades[2])/len(grades[2])          #средняя оценка третьего студента
+average4 = sum(grades[3])/len(grades[3])          #средняя оценка четвёртого студента
+average5 = sum(grades[4])/len(grades[4])          #средняя оценка пятого студента
+students = list(students)                         #перевод множества в список
+students.sort()                                   #сортировка списка по алфавиту
+dict = dict([[students[0],average1], [students[1],average2], [students[2],average3], [students[3],average4], [students[4],average5]])  #составление словаря
+print(dict)
