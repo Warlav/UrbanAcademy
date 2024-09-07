681ce1d573126f88171d28bb60477bb55f0b9a51
Warlav <warlav@gmail.com>
2024-09-07 14:27:20 +0300

    Create module_5_3.py

---
+++ module_5_3.py
@@ -0,0 +1,109 @@
+class House:
+  def __init__(self, name, number_of_floors):
+    self.name = name
+    self.number_of_floors = number_of_floors
+
+  def go_to(self, new_floor):
+    self.new_floor = new_floor
+    b = int(new_floor)
+    if b <= self.number_of_floors and b >= 1:
+      for i in range(1, b+1):
+        print(i)
+    else:
+      print('Такого этажа не существует')
+
+  def __len__(self):
+    return self.number_of_floors
+
+  def __str__(self):
+    return f'Название: {self.name}, кол-во этажей: {self.number_of_floors}'
+
+  def __eq__(self, other):
+    if isinstance(other, House):
+      return self.number_of_floors == other.number_of_floors
+    elif isinstance(other, int):
+      return self.number_of_floors == other
+    else:
+      print('Неверный тип данных', other)
+
+  def __lt__(self, other):
+    if isinstance(other, House):
+      return self.number_of_floors < other.number_of_floors
+    elif isinstance(other, int):
+      return self.number_of_floors < other
+    else:
+      print('Неверный тип данных', other)
+
+  def __le__(self, other):
+    if isinstance(other, House):
+      return self.number_of_floors <= other.number_of_floors
+    elif isinstance(other, int):
+      return self.number_of_floors <= other
+    else:
+      print('Неверный тип данных', other)
+
+  def __gt__(self, other):
+    if isinstance(other, House):
+      return self.number_of_floors > other.number_of_floors
+    elif isinstance(other, int):
+      return self.number_of_floors > other
+    else:
+      print('Неверный тип данных', other)
+
+  def __ge__(self, other):
+    if isinstance(other, House):
+      return self.number_of_floors >= other.number_of_floors
+    elif isinstance(other, int):
+      return self.number_of_floors >= other
+    else:
+      print('Неверный тип данных', other)
+
+  def __ne__(self, other):
+    if isinstance(other, House):
+      return self.number_of_floors != other.number_of_floors
+    elif isinstance(other, int):
+      return self.number_of_floors != other
+    else:
+      print('Неверный тип данных', other)
+
+  def __add__(self, value):
+    if isinstance(value, House):
+      self.number_of_floors += value.number_of_floors
+      return self
+    elif isinstance(value, int):
+      self.number_of_floors += value
+      return self
+    else:
+      print('Неверный тип данных', value)
+
+  def __radd__(self, value):
+    self.__add__(value)
+    return self
+
+  def __iadd__(self, value):
+    self.__add__(value)
+    return self
+
+h1 = House('ЖК Эльбрус', 10)
+h2 = House('ЖК Акация', 20)
+
+print(h1)
+print(h2)
+
+print(h1 == h2) # __eq__
+
+h1 = h1 + 10 # __add__
+print(h1)
+print(h1 == h2)
+
+h1 += 10 # __iadd__
+print(h1)
+
+h2 = 10 + h2 # __radd__
+print(h2)
+
+print(h1 > h2) # __gt__
+print(h1 >= h2) # __ge__
+print(h1 < h2) # __lt__
+print(h1 <= h2) # __le__
+print(h1 != h2) # __ne__
