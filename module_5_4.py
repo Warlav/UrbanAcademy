3a096991335e0c70319bcd3c7ab3c7ecdce86c6c
Warlav <warlav@gmail.com>
2024-09-12 14:18:19 +0300

    Кнопки больше

--- module_5_4.py
+++ module_5_4.py
@@ -3,7 +3,7 @@ class House:
 
     def __new__(cls, *args, **kwargs):
         cls.houses_history.append(args[0])
-        return super().__new__(cls)
+        return object.__new__(cls)
 
     def __init__(self, name, number_of_floors):
         self.name = name
