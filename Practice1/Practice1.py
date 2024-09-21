ecf494174b1b495e7d170fc33471f72f562fe114
Warlav <warlav@gmail.com>
2024-09-21 20:10:13 +0300

    Кнопки больше

--- Practice1/Practice1.py
+++ Practice1/Practice1.py
@@ -2,15 +2,18 @@ class User:
     """
     Класс пользователя, содержащий атрибуты: логин и пароль
     """
+
     def __init__(self, username, password, password_confirm):
         self.username = username
         if password == password_confirm:
             self.password = password
-        else:
-            print("Пароль не совпадает")
 
 
 class Database:
+    """
+    База данных логин и пароль
+    """
+
     def __init__(self):
         self.data = {}
 
@@ -21,9 +24,25 @@ class Database:
 if __name__ == '__main__':
     database = Database()
     while True:
-        choice = input('Приветствую! Выберите действие:\n1 - Вход\n2 - Регистрация\n3 - Выход')
-        user = User(input("Введите логин: "), password := input("Введите пароль: "), password2 := input("Повторите пароль: "))
-        if password != password2:
+        choice = input('Приветствую! Выберите действие:\n1 - Вход\n2 - Регистрация\n3 - Выход\n')
+        # if choice == 1:
+        #     login = input("Введите логин: ")
+        #     password = input("Введите пароль: ")
+        #     if login in database.data:
+        #         if password == database.data[login]:
+        #             print(f"Вход выполнен, {login}")
+        #             break
+        #         else:
+        #             print("Неверный пароль")
+        #     else:
+        #         print("Пользователь не найден")
+        # elif choice == 2:
+        #     user = User(input("Введите логин: "), password := input("Введите пароль: "),
+        #                 password2 := input("Повторите пароль: "))
+        #     if password != password2:
+        #         print("Пароли не совпадают. Попробуйте ещё раз")
+        #         continue
+        #     database.add_user(user.username, user.password)
+        if choice == 3:
             exit()
-        database.add_user(user.username, user.password)
         print(database.data)
