1a28826fbd3cfda06b18f6a882b6ce35f01e96e3
Warlav <warlav@gmail.com>
2024-09-27 21:39:17 +0300

    Кнопки больше

--- module_6_3.py
+++ module_6_3.py
@@ -1,33 +1,36 @@
 class Horse:
     def __init__(self, x_distance = 0, sound = 'Frrr'):
-        self.x_distance = x.distance
+        self.x_distance = x_distance
         self.sound = sound
        
     def run(self, dx):
-        return self.x_distance += dx
+        self.x_distance += dx
 
        
 class Eagle:
-    def __init__(self, y_distance = 0, sound = 'I train, eat, sleep, and repeat')
+    def __init__(self, y_distance = 0, sound = 'I train, eat, sleep, and repeat'):
+        self.y_distance = y_distance
+        self.sound = sound
 
     def fly(self, dy):
-        return y_distance += dy
+        self.y_distance += dy
 
 
 class Pegasus(Horse, Eagle):
     def __init__(self):
-        return super().__init__(Horse, Eagle)
+        super().__init__()
+        Eagle.__init__(self)
 
     def move(self, dx, dy):
         self.run(dx)
         self.fly(dy)
 
     def get_pos(self):
-        pos = (x_distance, y_distance)
+        pos = (self.x_distance, self.y_distance)
         return pos
 
     def voice(self):
-        return f'{self.sound}'
+        print(f'{self.sound}')
 
 p1 = Pegasus()
 
