075531a62d6286196b582a85489e931d7c14e9c7
Warlav <warlav@gmail.com>
2024-09-28 21:22:16 +0300

    Кнопки больше

---
+++ Ping Pong/game.py
@@ -0,0 +1,75 @@
+import arcade
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGT = 600
+SCREEN_TITLE = 'Pong Game'
+
+
+class Ball(arcade.Sprite):
+    def __init__(self):
+        super().__init__('ball.png', 0.01)
+        self.change_x = 1
+        self.change_y = 1
+
+    def update(self):
+        self.center_x += self.change_x
+        self.center_y += self.center_y
+        if self.right >= SCREEN_WIDTH:
+            self.change_x = -self.change_x
+        if self.left <= 0:
+            self.change_x = -self.change_x
+        if self.top <= 0:
+            self.change_y = -self.change_y
+        if self.top >= SCREEN_HEIGT:
+            self.change_y = -self.change_y
+
+class Bar(arcade.Sprite):
+    def __init__(self):
+        super().__init__('bar.png', 0.1)
+
+    def update(self):
+        self.center_x += self.change_x
+        if self.right >= SCREEN_WIDTH:
+            self.right = SCREEN_WIDTH
+        if self.left <= 0:
+            self.left = 0
+
+
+class Game(arcade.Window):
+    def __init__(self, width, height, title):
+        super().__init__(width, height, title)
+        self.bar = Bar()
+        self.ball = Ball()
+        self.setup()
+
+    def setup(self):
+        self.bar.center_x = SCREEN_WIDTH / 2
+        self.bar.center_y = SCREEN_HEIGT / 5
+        self.ball.center_x = SCREEN_WIDTH / 2
+        self.ball.center_y = SCREEN_HEIGT / 2
+
+    def on_draw(self):
+        self.clear((255, 255, 255))
+        self.bar.draw()
+        self.ball.draw()
+
+    def update(self, delta_time: float):
+        if arcade.check_for_collision(self.bar, self.ball):
+            self.ball.change_y = -self.ball.change_y
+        self.ball.update()
+        self.bar.update()
+
+    def on_key_press(self, symbol: int, modifiers: int):
+        if symbol == arcade.key.RIGHT:
+            self.ball.change_x = 5
+        if symbol == arcade.key.LEFT:
+            self.ball.change_x = -5
+
+    def on_key_release(self, symbol: int, modifiers: int):
+        if symbol == arcade.key.RIGHT or symbol == arcade.key.LEFT:
+            self.bar.change_x = 0
+
+
+if __name__ == '__main__':
+    window = Game(SCREEN_WIDTH, SCREEN_HEIGT, SCREEN_TITLE)
+    arcade.run()
