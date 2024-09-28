d636cbf18cbfd60fb49e48c68397b816655891a8
Warlav <warlav@gmail.com>
2024-09-28 18:51:01 +0300

    Кнопки больше

--- game.py
+++ game.py
@@ -1,8 +1,18 @@
 import arcade
 
-class Game(arcade.Window):
-    pass
+SCREEN_WIDTH = 800
+SCREEN_HEIGT = 600
+SCREEN_TITLE = 'Pong Game'
 
-if __name == '__main__':
-    window = Game()
+class Bar(arcade.Sprite):
+    def __init__(self):
+        super().__init__('bar.png', 1.0)
+
+
+class Game(arcade.Window):
+    def on_draw(self):
+        self.clear((255, 255, 255))
+
+if __name__ == '__main__':
+    window = Game(SCREEN_WIDTH, SCREEN_HEIGT, SCREEN_TITLE)
     arcade.run()
