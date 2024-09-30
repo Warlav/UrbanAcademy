226de9fb5f51e12ebbc904d62389dd887c2a4ad5
Warlav <warlav@gmail.com>
2024-09-30 21:46:36 +0300

    Кнопки больше

---
+++ module_7_2.py
@@ -0,0 +1,20 @@
+def custom_write(file_name, strings):
+    strings_positions = {}
+    file = open(file_name, 'a', encoding='utf-8')
+    for string in strings:
+        point = file.tell()
+        file.write(f'{string}\n')
+        strings_positions[(strings.index(string)+1, point)] = f'{string}'
+    file.close()
+    return strings_positions
+
+info = [
+    'Text for tell.',
+    'Используйте кодировку utf-8.',
+    'Because there are 2 languages!',
+    'Спасибо!'
+    ]
+
+result = custom_write('test.txt', info)
+for elem in result.items():
+  print(elem)
