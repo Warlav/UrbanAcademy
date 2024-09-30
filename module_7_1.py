28f67caf432385decebdf786abf19864eb74fc4a
Warlav <warlav@gmail.com>
2024-09-30 14:59:58 +0300

    Update module_7_1.py

--- module_7_1.py
+++ module_7_1.py
@@ -20,6 +20,20 @@ class Shop:
     def add(self, *products):
         open(self.__file_name, w)
         file = self.__file_name.read()
-        
+        if Product().name not in file:
+            self.__file_name.write(Product())
+        else:
+            print(f'Продукт {Product().name} уже есть в магазине'
         close(self.__file_name)
 
+
+s1 = Shop()
+p1 = Product('Potato', 50.5, 'Vegetables')
+p2 = Product('Spaghetti', 3.4, 'Groceries')
+p3 = Product('Potato', 5.5, 'Vegetables')
+
+print(p2) # __str__
+
+s1.add(p1, p2, p3)
+
+print(s1.get_products())
