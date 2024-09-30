c6a80836dafcbfd17f7e461d4bafbc443b84ddd9
Warlav <warlav@gmail.com>
2024-09-30 21:51:41 +0300

    Кнопки больше

--- module_7_2.py
+++ module_7_2.py
@@ -1,6 +1,6 @@
 def custom_write(file_name, strings):
     strings_positions = {}
-    file = open(file_name, 'a', encoding='utf-8')
+    file = open(file_name, 'w', encoding='utf-8')
     for string in strings:
         point = file.tell()
         file.write(f'{string}\n')
