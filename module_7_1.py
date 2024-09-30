64a0ea3a7d81f536125b461e30b76dc8968d8e98
Warlav <warlav@gmail.com>
2024-09-30 20:54:31 +0300

    Кнопки больше

--- module_7_1.py
+++ module_7_1.py
@@ -1,5 +1,5 @@
 class Product:
-    
+
     def __init__(self, name, weight, category):
         self.name = name
         self.weight = weight
@@ -7,6 +7,7 @@ class Product:
 
     def __str__(self):
         return f'{self.name}, {self.weight}, {self.category}'
+
 
 class Shop():
     __file_name = 'products.txt'
@@ -33,7 +34,7 @@ p1 = Product('Potato', 50.5, 'Vegetables')
 p2 = Product('Spaghetti', 3.4, 'Groceries')
 p3 = Product('Potato', 5.5, 'Vegetables')
 
-print(p2) # __str__
+print(p2)  # __str__
 
 s1.add(p1, p2, p3)
 
