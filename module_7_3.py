30c502efd4009e11f433e64a9bf24df5a9186e62
Warlav <warlav@gmail.com>
2024-10-03 01:08:17 +0300

    Кнопки больше

--- module_7_3.py
+++ module_7_3.py
@@ -1,20 +1,28 @@
 class WordsFinder:
     def __init__(self, *names):
-        self.__file_index = 1
         self.file_names = []
         self.file_names.append(*names)
-        file = open(f'file{self.__file_index}.txt', 'x', encoding='utf-8')
-        file.close()
-        self.__file_index += 1
-        
+        for name in names:
+            file = open(name, 'w', encoding='utf-8')
+            file.close()
 
     def get_all_words(self):
-        all_word = {}
-        all_word[self] = [*open(self)]
+        all_words = {}
+        words = []
+        replace_symbols = [',', '.', '=', '!', '?', ';', ':', ' - ']
+        for name in self.file_names:
+            with open(name, 'r', encoding='utf-8') as file:
+                for line in file:
+                    for symbol in replace_symbols:
+                        line.replace(symbol, '')
+                    words.append(line.lower().split())
+                all_words[name] = [words]
 
+    def find(self, word):
+        return {key: word}
 
 
 finder2 = WordsFinder('test_file.txt')
-print(finder2.get_all_words()) # Все слова
-print(finder2.find('TEXT')) # 3 слово по счёту
-print(finder2.count('teXT')) # 4 слова teXT в тексте всего
+print(finder2.get_all_words())  # Все слова
+print(finder2.find('TEXT'))  # 3 слово по счёту
+print(finder2.count('teXT'))  # 4 слова teXT в тексте всего
