e3e4658622c67b8d73ccd0405f2462a9d3e4f322
Warlav <warlav@gmail.com>
2024-10-05 22:55:51 +0300

    Да

--- module_7_4.py
+++ module_7_4.py
@@ -6,6 +6,7 @@ team1_time = 1552.512
 team2_time = 2153.31451
 tasks_total = score_1 + score_2
 time_avg = round((team1_time + team2_time) / tasks_total, 1)
+
 if score_1 > score_2 or score_1 == score_2 and team1_time > team2_time:
     challenge_result = 'Победа команды Мастера кода!'
 elif score_1 < score_2 or score_1 == score_2 and team1_time < team2_time:
