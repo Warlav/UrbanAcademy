84ef921568a2484d4441a790cb81721e700dc97f
Warlav <warlav@gmail.com>
2024-10-13 16:46:13 +0300

    Да

--- module7/module_7_5.py
+++ module7/module_7_5.py
@@ -10,7 +10,7 @@ for root, dirs, files in os.walk(root):
             filetime = os.path.getmtime(file)
             formatted_time = time.strftime("%d.%m.%Y %H:%M", time.localtime(filetime))
             filesize = os.path.getsize(file)
-            parent_dir = os.path.dirname(dir_)
+            parent_dir = os.path.dirname(filepath)
             print(
                 f'Обнаружен файл: {file}, Путь: {filepath}, Размер: {filesize} байт, Время изменения: {formatted_time},'
                 f' Родительская директория: {parent_dir}')
