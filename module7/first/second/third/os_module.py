bfa29c4a0524332448f1adee84c4e00796b2b7ba
Warlav <warlav@gmail.com>
2024-10-13 21:49:16 +0300

    Да

--- module7/first/second/third/os_module.py
+++ module7/first/second/third/os_module.py
@@ -1,16 +1,16 @@
 import os
 
 print(os.getcwd())
-if os.path.exists('first'):
-    os.chdir('first')
+if os.path.exists('../..'):
+    os.chdir('../..')
 else:
-    os.mkdir('first')
-    os.chdir('first')
+    os.mkdir('../..')
+    os.chdir('../..')
 print(os.getcwd())
 # os.makedirs(r'second\third')
 # for i in os.walk('.'):
 #     print(i)
-os.chdir(r'C:\Users\Warlav\PycharmProjects\UrbanAcademy\module7')
+os.chdir(r'/module7')
 print(os.getcwd())
 file = [f for f in os.listdir() if os.path.isfile(f)]
 dirs = [d for d in os.listdir() if os.path.isdir(d)]
