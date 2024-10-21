49661f97dfb3ef564157ce8f911a44fb893f179f
Warlav <warlav@gmail.com>
2024-10-21 21:38:45 +0300

    Да

--- explorer.py
+++ explorer.py
@@ -1,4 +1,12 @@
 import tkinter
+from tkinter import filedialog
+
+
+def file_select():
+    filename = filedialog.askopenfilename(initialdir="/", title="Выберите файл",
+                                          filetypes=(('Текстовый файл', '.txt'),
+                                                     ('Все файлы', '.*')))
+
 
 window = tkinter.Tk()
 window.title('Проводник')
@@ -7,7 +15,8 @@ window.configure(bg='light blue')
 window.resizable(False, False)
 text = tkinter.Label(window, text='Файл:', height=5, width=20, background='silver')
 text.grid(column=1, row=1)
-button_select = tkinter.Button(window, width=20, height=3, text='Выбрать файл')
+button_select = tkinter.Button(window, width=20, height=3, text='Выбрать файл', background='silver',
+                               foreground='blue', command=file_select)
 button_select.grid(column=1, row=2)
 
 window.mainloop()
