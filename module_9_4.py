72dce70ba38a64ea38dfbccdcb90a9aa158a78f1
Warlav <warlav@gmail.com>
2024-10-29 22:49:27 +0300

    Да

--- module_9_4.py
+++ module_9_4.py
@@ -10,12 +10,29 @@ print(list(map(lambda x, y: x == y, first, second)))
 
 def get_advanced_writer(file_name):
     def write_everything(*data_set):
-        with open(file_name, 'a') as file:
+        with open(file_name, 'a', encoding='utf-8') as file:
             for line in data_set:
-                file.write(f'{line}\n')
+                file.write(f'{str(line)}\n')
         return file
+
     return write_everything
 
 
 write = get_advanced_writer('example.txt')
 write('Это строчка', ['А', 'это', 'уже', 'число', 5, 'в', 'списке'])
+
+
+from random import choice
+class MysticBall:
+    def __init__(self, *words):
+        self.words = [*words]
+
+    def __call__(self):
+        word = choice(self.words)
+        return word
+
+
+first_ball = MysticBall('Да', 'Нет', 'Наверное')
+print(first_ball())
+print(first_ball())
+print(first_ball())
