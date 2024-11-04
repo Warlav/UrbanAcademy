8fe0857a4f9439cd72099a0daf2eb5815331f7dc
Warlav <warlav@gmail.com>
2024-11-04 15:02:57 +0300

    Да

--- task1.py
+++ task1.py
@@ -5,9 +5,9 @@ print(9 ** 0.5 * 5)
 print(9.99 > 9.98 and 1000 != 1000.1)
 
 # "3rd program":
-print(2*2+2)
-print(2*(2+2))
-print((2*2+2) == (2*(2+2)))
+print(2 * 2 + 2)
+print(2 * (2 + 2))
+print(2 * 2 + 2 == (2 * (2 + 2)))
 
 # "4th program":
-print(int(123.456*10)%10)
+print(int(123.456 * 10) % 10)
