4c311b023cad45a58f4b0cb8722fcd593ce2257b
Warlav <warlav@gmail.com>
2024-11-24 01:16:30 +0300

    Update module_10_4.py

--- module_10_4.py
+++ module_10_4.py
@@ -21,33 +21,33 @@ class Guest(threading.Thread):
 
 class Cafe:
     def __init__(self, *tables):
-        self.queue = queue.Queue(maxsize=len(tables))
+        self.queue = queue.Queue()
         self.tables = [*tables]
 
     def guest_arrival(self, *guests):
-        for table in self.tables:
-            for guest in guests:
+        for guest in guests:
+            for table in self.tables:
                 if table.guest is None:
                     table.guest = guest
                     print(f'{guest.name} сел(-а) за стол номер {table.number}')
                     guest.start()
-                else:
-                    self.queue.put(guest)
-                    print(f'{guest.name} в очереди')
+                    break
+            else:
+                self.queue.put(guest)
+                print(f'{guest.name} в очереди')
+                
 
     def discuss_guests(self):
-        for table in self.tables:
-            if not self.queue.empty() and table is not None:
-                if not table.guest.is_alive():
+        while any(table.guest is not None for table in self.tables):
+            for table in self.tables:
+                if table.guest is not None and not table.guest.is_alive():
                     print(f'{table.guest.name} покушал(-а) и ушёл(ушла)')
                     print(f'Стол номер {table.number} свободен')
                     table.guest = None
-            elif not self.queue.empty() and table is None:
-                table.guest = self.queue.get()
-                print(f'{table.guest} вышел(-ла) из очереди и сел(-а) за стол номер {table.number}')
-                table.guest.start()
-
-
+                if not self.queue.empty() and table.guest is None:
+                    table.guest = self.queue.get()
+                    print(f'{table.guest.name} вышел(-ла) из очереди и сел(-а) за стол номер {table.number}')
+                    table.guest.start()
 
 
 # Создание столов
