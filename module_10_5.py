a9b82fdc6e493a7d132c321605eabea0dff32633
Warlav <warlav@gmail.com>
2024-11-24 18:30:01 +0300

    Create module_10_5.py

---
+++ module_10_5.py
@@ -0,0 +1,27 @@
+import time
+import multiprocessing
+
+
+def read_info(name):
+  all_data = []
+  with open(name, 'r', encoding='utf-8') as file:
+    while file.readline() is not None:
+      all_data.append(file.readline())
+
+
+filenames = [f'./file {number}.txt' for number in range(1, 5)]
+
+# Линейный вызов
+start_time = time.time()
+for file in filenames:
+  read_info(file)
+print(time.time() - start_time)
+
+# Многопроцессный
+if __name__ == '__main__':
+  start_time = time.time()
+  with multiprocessing.Pool() as pool:
+    pool.map(read_info, filenames)
+    pool.join()
+  print(time.time() - start_time)
+  
