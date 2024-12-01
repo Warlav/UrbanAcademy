bfee1b3768aa72c27247e2008e79bd86b7e72c46
Warlav <warlav@gmail.com>
2024-12-02 01:12:39 +0300

    Update module_2_hard.py

--- module_2_hard.py
+++ module_2_hard.py
@@ -6,7 +6,5 @@ for i in range(1, fst_num):
     if fst_num % (i + j) == 0 and i < j:
       snd_num.append(str(i))
       snd_num.append(str(j))
-    else:
-      continue
 print('Первый камень: ', fst_num)
 print('Второй камень: ', ''.join(snd_num))
