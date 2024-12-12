19b7a9628ccef16737490d675cb91195201b8ab4
Warlav <warlav@gmail.com>
2024-12-13 00:45:46 +0300

    Да

--- introspection.py
+++ introspection.py
@@ -2,8 +2,9 @@ import inspect
 
 
 def introspection_info(obj):
+    attr = [getattr(obj, x) for x in dir(obj) if hasattr(obj, x)]
     meths = [x for x in dir(obj) if callable(x)]
-    return f'type: {type(obj)}, attributes: {dir(obj)}, methods: {meths}, module: {inspect.getmodule(obj)}'
+    return f'type: {type(obj)}, attributes({len(attr)}): {attr}, methods({len(meths)}): {meths}, module: {inspect.getmodule(obj)}'
 
 
 number_info = introspection_info(42)
