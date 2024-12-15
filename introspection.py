0157b0f080b1f0106f832b7282850c5ec6401ded
Warlav <warlav@gmail.com>
2024-12-16 01:25:01 +0300

    Да

--- introspection.py
+++ introspection.py
@@ -1,6 +1,16 @@
 import inspect
 
 
+class Abyrvalg:
+    first = 1
+
+    def __init__(self):
+        self.second = 2
+
+    def third(self):
+        pass
+
+
 def introspection_info(obj):
     attrs = [x for x in dir(obj) if not callable(getattr(obj, x))]
     methods = [x for x in dir(obj) if callable(getattr(obj, x))]
@@ -8,11 +18,15 @@ def introspection_info(obj):
     return (f'type: {type(obj)}\n'
             f'all attributes and methods({len(dir(obj))}): {dir(obj)}\n'
             f'attributes({len(attrs)}): {attrs}\n'
+            f'attribute values: {[x for x in obj.__dict__.keys() if not x.startswith('_')]}\n'
             f'methods({len(methods)}): {methods}\n'
             f'non dunder methods ({len(non_dunder_methods)}): {non_dunder_methods}\n'
             f'module: {inspect.getmodule(obj)}'
             )
 
 
-number_info = introspection_info(42)
-print(number_info)
+# number_info = introspection_info(42)
+# print(number_info)
+print()
+dog = Abyrvalg()
+print(introspection_info(dog))
