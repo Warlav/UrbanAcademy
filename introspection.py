6e725f039e0d64f442616a8b10558ce86f184ba4
Warlav <warlav@gmail.com>
2024-12-16 00:58:42 +0300

    Да

--- introspection.py
+++ introspection.py
@@ -1,24 +1,18 @@
-import builtins
 import inspect
 
 
-class Abyr:
-    first = 1
-
-    def __init__(self):
-        self.second = 2
-
-    def third(self):
-        pass
-
-
 def introspection_info(obj):
-    # print(hasattr(obj, '__name__'))
-    meths = [x for x in dir(obj) if inspect.ismethod(x)]
-    return (f'type: {type(obj)}\nattributes({len(dir(obj))}): {dir(obj)}\n'
-            f'methods({len(meths)}): {meths}\nmodule: {inspect.getmodule(obj)}')
+    attrs = [x for x in dir(obj) if not callable(getattr(obj, x))]
+    methods = [x for x in dir(obj) if callable(getattr(obj, x))]
+    non_dunder_methods = [x for x in dir(obj) if callable(getattr(obj, x)) and not x.startswith('__')]
+    return (f'type: {type(obj)}\n'
+            f'all attributes and methods({len(dir(obj))}): {dir(obj)}\n'
+            f'attributes({len(attrs)}): {attrs}\n'
+            f'methods({len(methods)}): {methods}\n'
+            f'non dunder methods ({len(non_dunder_methods)}): {non_dunder_methods}\n'
+            f'module: {inspect.getmodule(obj)}'
+            )
 
 
-fourth = Abyr()
-number_info = introspection_info(fourth)
+number_info = introspection_info(42)
 print(number_info)
