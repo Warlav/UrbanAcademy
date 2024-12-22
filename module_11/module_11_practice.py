d6ff8967989ce95b250af0aa3aca00ffb6790519
Warlav <warlav@gmail.com>
2024-12-22 14:49:51 +0300

    Да

--- module_11/module_11_practice.py
+++ module_11/module_11_practice.py
@@ -1,14 +1,25 @@
 from PIL import Image
+from PIL import ImageDraw, ImageFont
 
 
-def new_photo(name):
-    image = Image.open(name)
-    w, h = image.size
-    return image.resize((w // 2, h // 2))
+class PostMaker:
+    def __init__(self, name_photo):
+        self.image = Image.open(name_photo)
+        self.w, self.h = self.image.size
+        self.image = self.image.resize((self.w // 2, self.h // 2))
+
+    def paste(self, name_photo):
+        paste_image = Image.open(name_photo)
+        paste_image = paste_image.resize((paste_image.size[0] // 2, paste_image.size[1] // 2))
+        self.image.paste(paste_image, (200, 350))
+
+    def upgrade(self, text):
+        draw = ImageDraw.Draw(self.image)
+        font = ImageFont.truetype('arial.ttf', 30)
+        draw.text((0, 0), text, font=font, fill='red')
+        self.image.show()
 
 
-im = new_photo('Godfather.jpg')
-im_2 = new_photo('Design.jpeg')
-
-im.paste(im_2, (150, 300))
-im.show('MyPhoto')
+image = PostMaker('Godfather.jpg')
+image.paste('Design.jpeg')
+image.upgrade('Ты делаешь дизайн без уважения')
