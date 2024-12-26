a3a9b89a4876b43e08e6e7561b136974ceec10fa
Warlav <warlav@gmail.com>
2024-12-26 21:01:02 +0300

    Да

---
+++ module_12/suite_12_3.py
@@ -0,0 +1,23 @@
+import unittest
+import tests_12_1
+import tests_12_2
+
+suite_test = unittest.TestSuite()
+suite_test.addTest(unittest.TestLoader().loadTestsFromTestCase(tests_12_1.RunnerTest))
+suite_test.addTest(unittest.TestLoader().loadTestsFromTestCase(tests_12_2.TournamentTest))
+
+runner = unittest.TextTestRunner(verbosity=2)
+
+
+class RunnerTest(tests_12_1.RunnerTest):
+    @classmethod
+    def setUpClass(cls):
+        is_frozen = False
+
+
+class TournamentTest(tests_12_2.TournamentTest):
+    @classmethod
+    def setUpClass(cls):
+        is_frozen = True
+
+
