dfae6aa483aeb2e05bac656a8b5d9e2c5fd352b3
Warlav <warlav@gmail.com>
2024-12-27 20:39:50 +0300

    Да

---
+++ module_12/tests_12_4.py
@@ -0,0 +1,95 @@
+import logging
+import unittest
+
+
+class Runner:
+    def __init__(self, name, speed=5):
+        if isinstance(name, str):
+            self.name = name
+        else:
+            raise TypeError(f'Имя может быть только строкой, передано {type(name).__name__}')
+        self.distance = 0
+        if speed > 0:
+            self.speed = speed
+        else:
+            raise ValueError(f'Скорость не может быть отрицательной, сейчас {speed}')
+
+    def run(self):
+        self.distance += self.speed * 2
+
+    def walk(self):
+        self.distance += self.speed
+
+    def __str__(self):
+        return self.name
+
+    def __repr__(self):
+        return self.name
+
+    def __eq__(self, other):
+        if isinstance(other, str):
+            return self.name == other
+        elif isinstance(other, Runner):
+            return self.name == other.name
+
+
+class Tournament:
+    def __init__(self, distance, *participants):
+        self.full_distance = distance
+        self.participants = list(participants)
+
+    def start(self):
+        finishers = {}
+        place = 1
+        while self.participants:
+            for participant in self.participants:
+                participant.run()
+                if participant.distance >= self.full_distance:
+                    finishers[place] = participant
+                    place += 1
+                    self.participants.remove(participant)
+
+        return finishers
+
+
+class RunnerTest(unittest.TestCase):
+    is_frozen = False
+
+    @unittest.skipIf(is_frozen, 'Тесты в этом кейсе заморожены')
+    def test_walk(self):
+        try:
+            walker = Runner('Stepan', -5)
+        except ValueError as err:
+            logging.error('Включил заднюю пешеходную')
+        else:
+            [walker.walk() for i in range(10)]
+            self.assertEqual(walker.distance, 50)
+            logging.info('"test_walk" выполнен успешно')
+
+
+    @unittest.skipIf(is_frozen, 'Тесты в этом кейсе заморожены')
+    def test_run(self):
+        walker = Runner('Stepan')
+        [walker.run() for i in range(10)]
+        self.assertEqual(walker.distance, 100)
+
+    @unittest.skipIf(is_frozen, 'Тесты в этом кейсе заморожены')
+    def test_challenge(self):
+        walker_1 = Runner('Stepan')
+        walker_2 = Runner('Oleg')
+        [walker_1.walk() for i in range(10)]
+        [walker_2.run() for i in range(10)]
+        self.assertNotEqual(walker_1.distance, walker_2.distance)
+
+
+# first = Runner('Вося', 10)
+# second = Runner('Илья', 5)
+# third = Runner('Арсен', 10)
+#
+# t = Tournament(101, first, second)
+# print(t.start())
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level='INFO', filename='runner_tests.log', filemode='w', encoding='utf-8',
+                        format='%(asctime)s | %(levelname)s | %(message)')
