2b8a1a5d4cbcbcea5d2ff086550ccab0e907c42c
Warlav <warlav@gmail.com>
2024-12-27 22:22:11 +0300

    Да

--- module_12/tests_12_4.py
+++ module_12/tests_12_4.py
@@ -95,5 +95,7 @@ print(t.start())
 
 
 if __name__ == '__main__':
-    logging.basicConfig(level='INFO', filemode='w', filename='runner_tests.log', encoding='utf-8',
-                        format='%(asctime)s | %(levelname)s | %(message)')
+    logging.basicConfig(level=logging.INFO, filemode='w', filename='runner_tests.log',
+                        encoding='utf-8', format='%(asctime)s - %(levelname)s : %(message)s')
+    logging.root.setLevel(logging.NOTSET)
+    unittest.main()
