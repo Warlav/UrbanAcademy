524e112c662e4fb0a25549ac193cb80a4db96728
Warlav <warlav@gmail.com>
2024-12-27 19:39:45 +0300

    Да

--- module_12/tests_12_3.py
+++ module_12/tests_12_3.py
@@ -4,23 +4,21 @@ import runner_and_tournament
 
 
 class RunnerTest(unittest.TestCase):
-    @classmethod
-    def setUpClass(cls):
-        cls.is_frozen = False
+    is_frozen = False
 
-    @unittest.skipIf(cls.is_frozen, 'Тесты в этом кейсе заморожены')
+    @unittest.skipIf(is_frozen, 'Тесты в этом кейсе заморожены')
     def test_walk(self):
         walker = Runner('Stepan')
         [walker.walk() for i in range(10)]
         self.assertEqual(walker.distance, 50)
 
-    @unittest.skipIf(cls.is_frozen, 'Тесты в этом кейсе заморожены')
+    @unittest.skipIf(is_frozen, 'Тесты в этом кейсе заморожены')
     def test_run(self):
         walker = Runner('Stepan')
         [walker.run() for i in range(10)]
         self.assertEqual(walker.distance, 100)
 
-    @unittest.skipIf(cls.is_frozen, 'Тесты в этом кейсе заморожены')
+    @unittest.skipIf(is_frozen, 'Тесты в этом кейсе заморожены')
     def test_challenge(self):
         walker_1 = Runner('Stepan')
         walker_2 = Runner('Oleg')
@@ -30,10 +28,11 @@ class RunnerTest(unittest.TestCase):
 
 
 class TournamentTest(unittest.TestCase):
+    is_frozen = True
+
     @classmethod
     def setUpClass(cls):
         cls.all_results = {}
-        cls.is_frozen = True
 
     def setUp(self):
         self.runner_1 = runner_and_tournament.Runner('Усэйн', 10)
@@ -44,30 +43,34 @@ class TournamentTest(unittest.TestCase):
     def tearDownClass(cls):
         {print(f'\n{i}:'): [print(f'{k}: ', v) for k, v in j.items()] for i, j in cls.all_results.items()}
 
-    @unittest.skipIf(cls.is_frozen, 'Тесты в этом кейсе заморожены')
+    @unittest.skipIf(is_frozen, 'Тесты в этом кейсе заморожены')
     def test_zabeg_1(self):
         run = runner_and_tournament.Tournament(90, self.runner_1, self.runner_3).start()
         TournamentTest.all_results['test_zabeg_1'] = run
         keys = list(run.keys())
         self.assertTrue(run[keys[-1]].name == self.runner_3.name)
 
-    @unittest.skipIf(cls.is_frozen, 'Тесты в этом кейсе заморожены')
+    @unittest.skipIf(is_frozen, 'Тесты в этом кейсе заморожены')
     def test_zabeg_2(self):
         run = runner_and_tournament.Tournament(90, self.runner_2, self.runner_3).start()
         TournamentTest.all_results['test_zabeg_2'] = run
         keys = list(run.keys())
         self.assertTrue(run[keys[-1]].name == self.runner_3.name)
 
-    @unittest.skipIf(cls.is_frozen, 'Тесты в этом кейсе заморожены')
+    @unittest.skipIf(is_frozen, 'Тесты в этом кейсе заморожены')
     def test_zabeg_3(self):
         run = runner_and_tournament.Tournament(90, self.runner_1, self.runner_2, self.runner_3).start()
         TournamentTest.all_results['test_zabeg_3'] = run
         keys = list(run.keys())
         self.assertTrue(run[keys[-1]].name == self.runner_3.name)
 
-    @unittest.skipIf(cls.is_frozen, 'Тесты в этом кейсе заморожены')
+    @unittest.skipIf(is_frozen, 'Тесты в этом кейсе заморожены')
     def test_zabeg_fail(self):
         run = runner_and_tournament.Tournament(6, self.runner_1, self.runner_2, self.runner_3).start()
         TournamentTest.all_results['test_zabeg_fail'] = run
         keys = list(run.keys())
         self.assertTrue(run[keys[-1]].name == self.runner_3.name)
+
+
+if __name__ == '__main__':
+    unittest.main()
