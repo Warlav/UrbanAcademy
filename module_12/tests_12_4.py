a0dce28512fd873f49a293ed3f68023aea383287
Warlav <warlav@gmail.com>
2025-01-01 17:46:55 +0300

    Да

--- module_12/tests_12_4.py
+++ module_12/tests_12_4.py
@@ -94,7 +94,7 @@ class RunnerTest(unittest.TestCase):
 # print(t.start())
 
 logging.basicConfig(level=logging.INFO, filemode='w', filename='runner_tests.log',
-                        encoding='utf-8', format='%(asctime)s - %(levelname)s : %(message)s')
+                    encoding='utf-8', format='%(asctime)s - %(levelname)s : %(message)s')
 
 if __name__ == '__main__':
     unittest.main()
