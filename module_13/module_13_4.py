58160a5d2ac0cc0bf3c4b677bdccd1dde95ecd4a
Warlav <warlav@gmail.com>
2025-01-04 21:45:08 +0300

    Да

--- module_13/module_13_4.py
+++ module_13/module_13_4.py
@@ -1,8 +1,6 @@
-from aiogram import Bot, Dispatcher, executor, types
+from aiogram import Bot, Dispatcher, executor
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher.filters.state import State, StatesGroup
-from aiogram.dispatcher import FSMContext
-import asyncio
 
 api = 'token'
 bot = Bot(token=api)
