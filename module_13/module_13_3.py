150eb0617fdadd74803c82fecbfdd21a4be60592
Warlav <warlav@gmail.com>
2025-01-04 20:24:29 +0300

    Да

--- module_13/module_13_3.py
+++ module_13/module_13_3.py
@@ -2,26 +2,19 @@ from aiogram import Bot, Dispatcher, executor, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 import asyncio
 
-api = '6779281764:AAGTESGuMFxA1mUcVBDFa-yrHmBLi_zizmU'
+api = 'token'
 bot = Bot(token=api)
 dp = Dispatcher(bot, storage=MemoryStorage())
 
 
-@dp.message_handler(text=['Привет', 'привет', 'Hi', 'hi', 'Hello', 'hello'])
-async def hi_message(message):
-    print('Получили привет')
-
-
 @dp.message_handler(commands=['start'])
 async def start(message):
-    print('Привет! Я бот помогающий твоему здоровью.')
-    await message.answer('Рады вас видеть в нашем боте.')
+    await message.answer('Привет! Я бот помогающий твоему здоровью.')
 
 
 @dp.message_handler()
 async def all_massages(message):
-    print('Введите команду /start, чтобы начать общение.')
-    await message.answer(message.text)
+    await message.answer('Введите команду /start, чтобы начать общение.')
 
 
 if __name__ == '__main__':
