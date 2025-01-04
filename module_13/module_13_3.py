0083be6d23da21ae6c9a4a8d0a7732b486f81b48
Warlav <warlav@gmail.com>
2025-01-04 20:10:56 +0300

    Да

---
+++ module_13/module_13_3.py
@@ -0,0 +1,26 @@
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+import asyncio
+
+api = '6779281764:AAGTESGuMFxA1mUcVBDFa-yrHmBLi_zizmU'
+bot = Bot(token=api)
+dp = Dispatcher(bot, storage=MemoryStorage())
+
+
+@dp.message_handler(text=['Привет', 'привет', 'Hi', 'hi', 'Hello', 'hello'])
+async def hi_message(message):
+    print('Получили привет')
+
+
+@dp.message_handler(commands=['start'])
+async def start(message):
+    print('Привет! Я бот помогающий твоему здоровью.')
+
+
+@dp.message_handler()
+async def all_massages(message):
+    print('Введите команду /start, чтобы начать общение.')
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
